49c1dae3ede282e7568dbe97971c72bf6747753b
John <barretodiazjohnalexander@gmail.com>
2021-09-16 20:35:32 -0500

    Cambios tabla

--- Inventario.js
+++ Inventario.js
@@ -66,16 +66,15 @@ var tabla = document.getElementById('Tabla');
 db.collection("PRODUCTS").onSnapshot((querySnapshot) => {
     tabla.innerHTML='';
     querySnapshot.forEach((doc) => {
-        
         tabla.innerHTML += `
         <tr>
-            <th scope="row">${doc.data().Id}</th>
-            <td style="display:none">${doc.id} </td>
+            <td scope="row">${doc.data().Id}</td>
             <td>${doc.data().Nombre_del_producto}</td>
             <td>${doc.data().Precio}</td>
             <td>${doc.data().Productos_Disponibles}</td>
+            <td style="display:none">${doc.id} </td>
             <td> <button id="btn_Edit" onclick="editar('${doc.id}','${doc.data().Nombre_del_producto}','${doc.data().Precio}','${doc.data().Productos_Disponibles}')" class="btn"><b>EDITAR</b></button>  
-            <button id="btn_Delete" onclick="eliminar('${doc.id}')" class="btn btn-sm"> <i class="fas fa-trash-alt"></i> </button> </td>   
+            <button id="btn_Delete" onclick="eliminar('${doc.id}')" class="btn btn-sm"> <i class="fas fa-trash-alt"></i> </button> </td>
         </tr>
         `
     });
